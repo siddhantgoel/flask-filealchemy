93aed6267ccabe4edd0b9168bf9a7b3004893716
Siddhant Goel <me@sgoel.org>
2018-10-11 16:20:17 +0200

    basic implementation of loading data from files

--- flask_filealchemy/__init__.py
+++ flask_filealchemy/__init__.py
@@ -1,7 +1,10 @@
+import os
+import warnings
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from yaml import load
 
 
 class MissingModelsError(Exception):
@@ -15,6 +18,7 @@ class FileAlchemy:
         self._engine = create_engine('sqlite:///:memory:')
         self._sessionmaker = sessionmaker(bind=self._engine)
 
+        self._data_dir = self.app.config.get('FILEALCHEMY_DATA_DIR')
         self._models = self.app.config.get('FILEALCHEMY_MODELS')
 
         if not self._models:
@@ -23,7 +27,30 @@ class FileAlchemy:
         self._load_data()
 
     def _load_data(self):
-        pass
+        with self.session() as session:
+            for model in self.models:
+                path = os.path.join(self._data_dir, model.__name__)
+
+                if not os.path.isdir(path):
+                    continue
+
+                files = os.path.listdir(path)
+
+                for file_ in files:
+                    data = None
+
+                    try:
+                        with open(file_) as fd:
+                            data = fd.read()
+                    except IOError:
+                        warnings.warn('Unable to read {}'.format(file_))
+                        continue
+
+                    values = load(data)
+
+                    session.add(model(**values))
+
+            session.commit()
 
     @contextmanager
     def session(self):
