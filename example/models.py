bbae4ad9a1b5bb41691fe51046d1becbee6dae3d
Siddhant Goel <me@sgoel.org>
2018-10-11 08:48:34 +0200

    add __tablename__ attribute to example models

--- example/models.py
+++ example/models.py
@@ -5,11 +5,15 @@ Base = declarative_base()
 
 
 class Author(Base):
+    __tablename__ = 'authors'
+
     slug = Column(String(255), primary_key=True)
     name = Column(String(255), nullable=False)
 
 
 class Book(Base):
+    __tablename__ = 'books'
+
     slug = Column(String(255), primary_key=True)
     title = Column(String(255), nullable=False)
     author_slug = Column(String(255), ForeignKey('authors.slug'),
