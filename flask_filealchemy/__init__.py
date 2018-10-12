c26219f284f94f984fe8429f1ba872b59f75aa29
Siddhant Goel <me@sgoel.org>
2018-10-12 13:53:37 +0200

    commit objects based on foreign key dependency order

--- flask_filealchemy/__init__.py
+++ flask_filealchemy/__init__.py
@@ -13,19 +13,19 @@ class FileAlchemy:
         self._models = self.app.config.get('FILEALCHEMY_MODELS')
 
         if not self._models:
-            warnings.warn('Flask-FileAlchemy: no models found')
+            warnings.warn('flask-filealchemy: no models found')
 
     def load_data(self):
         self.db.create_all()
 
-        try:
-            session = self.db.session
+        for table in self.db.metadata.sorted_tables:
+            path = os.path.join(self._data_dir, table.name)
 
-            for model in self._models:
-                path = os.path.join(self._data_dir, model.__name__)
+            if not os.path.isdir(path):
+                continue
 
-                if not os.path.isdir(path):
-                    continue
+            try:
+                session = self.db.session
 
                 for file_ in os.listdir(path):
                     data = None
@@ -34,16 +34,30 @@ class FileAlchemy:
                         with open(os.path.join(path, file_)) as fd:
                             data = fd.read()
                     except IOError:
-                        warnings.warn('Unable to read {}'.format(file_))
+                        warnings.warn(
+                            'flask-filealchemy: unable to read {}'.format(
+                                file_))
                         continue
 
                     values = load(data)
 
-                    session.add(model(**values))
-        except Exception:
-            session.rollback()
-            raise
-        else:
-            session.commit()
-        finally:
-            session.close()
+                    kwargs = {
+                        column.name: values.get(column.name)
+                        for column in table.columns
+                    }
+
+                    model = self._model_for(table.name)
+
+                    session.add(model(**kwargs))
+            except Exception:
+                session.rollback()
+                raise
+            else:
+                session.commit()
+            finally:
+                session.close()
+
+    def _model_for(self, table_name):
+        for model in self._models:
+            if model.__tablename__ == table_name:
+                return model
