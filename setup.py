8d8ccb35fe7357ad9f1657cb3e1dec7f8fd317ab
Siddhant Goel <me@sgoel.org>
2018-10-31 17:55:52 +0100

    include Python 3.4 in travis config

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     install_requires=(
         'Flask-SQLAlchemy>=2.1',
     ),
-    python_requires='>=3.5.0',
+    python_requires='>=3.4.0',
     cmdclass={
         'publish': PublishCommand
     },
@@ -78,6 +78,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Flask',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
