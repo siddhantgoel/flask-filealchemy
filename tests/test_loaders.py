ebbd5ce9926c8d195b262aabdf7883d89a907b8d
Siddhant Goel <me@sgoel.org>
2020-08-29 17:03:53 +0200

    test: add tests for markdown frontmatter loader

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, String
 from flask_filealchemy.loaders import (
     BaseLoader,
     loader_for,
+    MarkdownFrontmatterDirectoryLoader,
     YAMLDirectoryLoader,
     YAMLFileLoader,
 )
@@ -99,4 +100,54 @@ def test_yaml_directory_loader_with_extra_extensions(db, tmpdir):
 
     assert isinstance(
         loader_for(Path(tmpdir.strpath), author_table), YAMLDirectoryLoader
+    )
+
+
+def test_markdown_frontmatter_loader(db, tmpdir):
+    authors = tmpdir.mkdir('authors')
+
+    authors.join('first.md').write('does-not-matter')
+    authors.join('second.md').write('does-not-matter')
+
+    class Author(db.Model):
+        __tablename__ = 'authors'
+
+        slug = Column(String(255), primary_key=True)
+        name = Column(String(255), nullable=False)
+
+    assert len(db.metadata.sorted_tables) == 1
+    assert db.metadata.sorted_tables[0].name == 'authors'
+
+    author_table = db.metadata.sorted_tables[0]
+
+    assert isinstance(
+        loader_for(Path(tmpdir.strpath), author_table),
+        MarkdownFrontmatterDirectoryLoader,
+    )
+
+
+def test_markdown_frontmatter_loader_with_extra_extensions(db, tmpdir):
+    authors = tmpdir.mkdir('authors')
+
+    for index, extension in enumerate(
+        MarkdownFrontmatterDirectoryLoader.extensions
+    ):
+        authors.join('authors-{}.{}'.format(index, extension)).write(
+            'does-not-matter'
+        )
+
+    class Author(db.Model):
+        __tablename__ = 'authors'
+
+        slug = Column(String(255), primary_key=True)
+        name = Column(String(255), nullable=False)
+
+    assert len(db.metadata.sorted_tables) == 1
+    assert db.metadata.sorted_tables[0].name == 'authors'
+
+    author_table = db.metadata.sorted_tables[0]
+
+    assert isinstance(
+        loader_for(Path(tmpdir.strpath), author_table),
+        MarkdownFrontmatterDirectoryLoader,
     )
